dcf9d40fc01e29526e9f5acb8b9d77b55f987c8c
isa <5445946+icortes@users.noreply.github.com>
2021-10-05 19:01:08 -0700

    pull merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,31 @@
-fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-popular-movies&page=1&year=2018", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
-		"x-rapidapi-key": "1acff560dfmshb97c3c2facb502cp19f4b7jsn8e70140c970f"
-	}
-})
-.then(response => {
-	console.log(response);
-    return response.json();
-})
+function getPopular(type, year) {
+	fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-popular-movies&page=1&year=2018", {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+				"x-rapidapi-key": "1acff560dfmshb97c3c2facb502cp19f4b7jsn8e70140c970f"
+			}
+		})
+		.then(response => {
+			console.log(response);
+			return response.json();
+		})
 
-.then(data => {
-    console.log(data);
-});
+		.then(data => {
+			console.log(data);
+		});
+}
+
 
 ombdkey = "726baba7";
 
 fetch("http://www.omdbapi.com/?apikey=" + ombdkey + "&i=tt1285016")
-.then(response => {
-    console.log(response);
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-})
+	.then(response => {
+		console.log(response);
+		return response.json();
+	})
+	.then(data => {
+		console.log(data);
+	})
+
+getPopular();
