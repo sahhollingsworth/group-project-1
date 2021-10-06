cbe2266039b8f20f8104de7d7534d0690751cb02
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-10-06 00:36:34 -0700

    Style: Added basic Bulma classes to DOM elements.
    Fix: Removed extraneous js to create book classes, books infeasible and will not be supported in MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,32 +67,3 @@ function getMediaInfo(imdbID) {
 }
 
 getPopular("get-popular-movies", 2021);
-
-// Google books API fetch
-fetch(`https://www.googleapis.com/books/v1/volumes?q=cat&key=AIzaSyDtUUPKatgp-DpuXV5xxSYrWkT9DLYlIc8`)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
-
-// NYT Fetch API
-    fetch(`https://api.nytimes.com/svc/books/v3/lists/'2021-02-02'/hardcover-fiction.json&api-key=nchTYYEyH5NAeLwlhujlR0i0GgClzSsd`)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log
-    });
-
-// class book {
-//     constructor(title, author, releaseDate, description) {
-//         this.title = title;
-//         this.author = author;
-//         this.releaseDate = releaseDate;
-//         this.description = description;
-//     }
-// }
-
